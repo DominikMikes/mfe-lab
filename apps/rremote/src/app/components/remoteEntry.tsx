807cc774a80120701b61c7dfc1f908a59aae0a64
Dominik Blau <dominik.blau@gmail.com>
2022-06-11 23:25:28 +0200

    removed not needed apps, updated readme

--- apps/rremote/src/app/components/remoteEntry.tsx
+++
@@ -1,8 +0,0 @@
-function RemoteEntry() {
-  return (
-      <div>
-          <h3>Remote Entry react app</h3>
-      </div>
-);
-}
-export default RemoteEntry;
--- apps/rshell/webpack.config.js
+++
@@ -1,3 +0,0 @@
-const { withModuleFederation } = require('@nrwl/react/module-federation');
-const config = require('./module-federation.config');
-module.exports = withModuleFederation(config);
